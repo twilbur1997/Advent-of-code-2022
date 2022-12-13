50b419bd4059488cb87d3c8928bb69f7eff2da07
Thomas Wilbur <twilbur@umich.edu>
2022-12-12 19:57:59 -0800

    Completed Day 3

--- 03_day/03_day.py
+++ 03_day/03_day.py
@@ -18,7 +18,6 @@ def convert_to_priority(character):
 def day_03_challenge_part_1():
     # Find the item type that appears in both compartments of each rucksack.
     # What is the sum of the priorities of those item types?
-
     input_file = "input_03_day.txt"
     total = 0
 
@@ -26,18 +25,24 @@ def day_03_challenge_part_1():
         line = file.readline().strip()
 
         while line:
-            middle = len(line)/2
+            middle = len(line)//2
             first_h = line[:middle]
             second_h = line[middle:]
+            for letter in first_h:
+                if letter in second_h:
+                    total += convert_to_priority(letter)
+                    break
 
-            total += convert_to_priority(shared_letter)
             line = file.readline().strip()
 
-    return 0
+    return total
 
 
 def day_03_challenge_part_2():
+    # Find the item type that corresponds to the badges of each three-Elf group.
+    # What is the sum of the priorities of those item types?
     input_file = "input_03_day.txt"
+    total = 0
 
     with open(input_file, "r") as file:
         prev_lines = []
@@ -45,9 +50,16 @@ def day_03_challenge_part_2():
 
         while line:
             prev_lines.append(line)
+            if len(prev_lines)==3:
+                for elf1_letter in prev_lines[0]:
+                    if elf1_letter in prev_lines[1] and elf1_letter in prev_lines[2]:
+                        total += convert_to_priority(elf1_letter)
+                        prev_lines = []
+                        break
+
             line = file.readline().strip()
 
-    return 0
+    return total
 
 
 def main():
