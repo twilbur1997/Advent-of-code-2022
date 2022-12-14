b6919b01750200c1b3fc0256b02bc910132e6322
Thomas Wilbur <twilbur@umich.edu>
2022-12-13 21:26:19 -0800

    Completed Day 05 part 1 successfully

--- 05_day/05_day.py
+++ 05_day/05_day.py
@@ -1,5 +1,33 @@
 import os
 
+stack0 = ''
+stack1 = 'WRTG'
+stack2 = 'WVSMPHCG'
+stack3 = 'MGSTLC'
+stack4 = 'FRWMDHJ'
+stack5 = 'JFWSHLQP'
+stack6 = 'SMFNDJP'
+stack7 = 'JSCGFDBZ'
+stack8 = 'BTR'
+stack9 = 'CLWNH'
+
+all_stacks = [stack0,stack1,stack2,stack3,stack4,stack5,stack6,stack7,stack8,stack9]
+
+
+def move_num_from_a_to_b(num, a, b):
+    for x in range(num):
+        '''
+        print(all_stacks[a][0])
+        print(all_stacks[a])
+        print(all_stacks[b], '\n')
+        '''
+        all_stacks[b] = all_stacks[a][0]+all_stacks[b]
+        all_stacks[a] = all_stacks[a][1:]
+        '''
+        print(all_stacks[a])
+        print(all_stacks[b],'\n')
+        '''
+
 
 def day_05_challenge_part_1():
     # After the rearrangement procedure completes, what crate ends up on top of each stack?
@@ -7,32 +35,76 @@ def day_05_challenge_part_1():
 
 
     with open(input_file, "r") as file:
-        prev_lines = []
-        line = int(file.readline().strip())
+        line = file.readline().strip()
+
+        while "move" not in line:
+            line = file.readline().strip()
 
         while line:
-            prev_lines.append(line)
-            line = int(file.readline().strip())
+            # move 3 from 4 to 3
+            split_line = line.split(' ')
+            num_moved = int(split_line[1])
+            from_pile = int(split_line[3])
+            to_pile = int(split_line[5])
+            move_num_from_a_to_b(num_moved, from_pile, to_pile)
 
-    return 0
+            line = file.readline().strip()
+
+    top_of_stacks = ''
+    for item in all_stacks[1:]:
+        top_of_stacks = top_of_stacks+item[0]
+
+    return top_of_stacks
+
+
+def move_num_from_a_to_b_multiple(num, a, b):
+    for x in range(num):
+        '''
+        print(all_stacks[a][0])
+        print(all_stacks[a])
+        print(all_stacks[b], '\n')
+        '''
+        all_stacks[b] = all_stacks[a][0]+all_stacks[b]
+        all_stacks[a] = all_stacks[a][1:]
+        '''
+        print(all_stacks[a])
+        print(all_stacks[b],'\n')
+        '''
 
 
 def day_05_challenge_part_2():
     input_file = "input_05_day.txt"
 
     with open(input_file, "r") as file:
-        prev_lines = []
-        line = int(file.readline().strip())
+        line = file.readline().strip()
+
+        while "move" not in line:
+            line = file.readline().strip()
 
         while line:
-            prev_lines.append(line)
-            line = int(file.readline().strip())
+            # move 3 from 4 to 3
+            split_line = line.split(' ')
+            num_moved = int(split_line[1])
+            from_pile = int(split_line[3])
+            to_pile = int(split_line[5])
+            move_num_from_a_to_b(num_moved, from_pile, to_pile)
 
-    return 0
+            line = file.readline().strip()
+
+    top_of_stacks = ''
+    for item in all_stacks[1:]:
+        top_of_stacks = top_of_stacks+item[0]
+
+    return top_of_stacks
 
 
 def main():
-    print(day_05_challenge_part_1())
+    '''
+    for item in all_stacks:
+        print(item)
+    return
+    '''
+    # print(day_05_challenge_part_1())
     print(day_05_challenge_part_2())
 
 
